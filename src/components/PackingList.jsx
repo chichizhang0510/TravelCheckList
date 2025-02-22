558abc3dfac24d0ec8c81e6f58baa9ec002afd5a
Chichi Zhang <chichizhang20000510@gmail.com>
2025-02-21 17:29:15 -0800

    add sort function

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,12 +1,35 @@
 import PropTypes from "prop-types";
 import Item from "./Item";
+import { useState } from "react";
 
 function PackingList({ items, onTogglePacked, removeItem }) {
-  console.log("onTogglePacked exists?", onTogglePacked);
+  const [sortBy, setSortBy] = useState("input");
+
+  function changeSort(e) {
+    setSortBy(e.target.value);
+  }
+
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -15,12 +38,28 @@ function PackingList({ items, onTogglePacked, removeItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={changeSort}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by decription order</option>
+          <option value="packed">Sort by packed order</option>
+        </select>
+      </div>
     </div>
   );
 }
 
 PackingList.propTypes = {
   items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      description: PropTypes.string.isRequired,
+      quantity: PropTypes.number.isRequired,
+      packed: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  sortedItems: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
       description: PropTypes.string.isRequired,
